5158263eec922e7ee7cbc25eb0d47ac75f4ce12b
Brixsta <Brixsta@gmail.com>
2023-08-14 15:25:27 -0600

    added bubblesort

--- Sort/Bubble Sort.js
+++ Sort/Bubble Sort.js
@@ -1,16 +1,14 @@
 const bubbleSort = (arr) => {
   for (let i = 0; i < arr.length; i++) {
-    for (let j = i + 1; j < arr.length; j++) {
-      if (arr[j] < arr[i]) {
-        let temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
       }
     }
   }
-
-  console.log(arr);
   return arr;
 };
 
-bubbleSort([2, 3, 4, 1, 22]);
+console.log(bubbleSort([10, 5, 4, 3, 2, 1, 6, 7, 9, 8]));
