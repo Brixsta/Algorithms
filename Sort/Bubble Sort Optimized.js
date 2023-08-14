fb7b9e5eb07c302893971c8dcf8451f8f8959833
Brixsta <Brixsta@gmail.com>
2023-08-14 15:28:46 -0600

    test

--- Sort/Bubble Sort Optimized.js
+++ Sort/Bubble Sort Optimized.js
@@ -5,9 +5,7 @@ const bubbleSort = (arr) => {
     for (let j = 0; j < arr.length - 1; j++) {
       console.log(arr, i);
       if (arr[j] > arr[j + 1]) {
-        let temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
+        swap(arr, j, j + 1);
         swapped = true;
       }
     }
@@ -15,5 +13,11 @@ const bubbleSort = (arr) => {
   }
   return arr;
 };
+
+function swap(arr, idx1, idx2) {
+  let temp = arr[idx1];
+  arr[idx1] = arr[idx2];
+  arr[idx2] = temp;
+}
 
 console.log(bubbleSort([10, 5, 4, 3, 2, 1, 6, 7, 9, 8]));
