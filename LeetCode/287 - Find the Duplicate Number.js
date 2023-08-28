f20bb776152a57299b6a14a2d54b5dadcefedc6b
Brixsta <Brixsta@gmail.com>
2023-08-28 11:00:52 -0600

    added leetcode 287

--- LeetCode/287 - Find the Duplicate Number.js
+++ LeetCode/287 - Find the Duplicate Number.js
@@ -1,26 +1,20 @@
-// LEETCODE 287
 var findDuplicate = function (nums) {
   let init = 0;
   let slow = 0;
   let fast = 0;
 
-  while (1) {
+  while (true) {
     fast = nums[nums[fast]];
     slow = nums[slow];
     if (fast === slow) break;
   }
 
-  while (1) {
-    console.log(init, slow);
+  while (true) {
     init = nums[init];
     slow = nums[slow];
     if (init === slow) break;
   }
-
-  // slow: 3
-  // init: 3
   return slow;
 };
-//floyds cyc
 
-console.log(findDuplicate([1, 4, 4, 3, 2]));
+console.log(findDuplicate([1, 2, 3, 4, 5, 2]));
