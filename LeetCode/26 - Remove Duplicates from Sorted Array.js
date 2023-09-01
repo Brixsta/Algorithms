e9be549fc58844dc33ed18cdbc14f9950fcaa32c
Brixsta <Brixsta@gmail.com>
2023-08-31 19:21:07 -0600

    added 209

--- LeetCode/26 - Remove Duplicates from Sorted Array.js
+++ LeetCode/26 - Remove Duplicates from Sorted Array.js
@@ -1,27 +1,20 @@
-var removeDuplicates = function (nums) {
-  let idx = 1;
-
-  for (let i = 0; i < nums.length - 1; i++) {
-    if (nums[i] !== nums[i + 1]) {
-      nums[idx] = nums[i + 1];
-      idx++;
+var minSubArrayLen = function (target, nums) {
+  let sum = 0;
+  let left = 0;
+  let min = Infinity;
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+    while (sum >= target) {
+      min = Math.min(min, i - left + 1);
+      sum -= nums[left];
+      left++;
     }
   }
-  return idx;
+  return min !== Infinity ? min : 0;
 };
 
-// var removeDuplicates = function (nums) {
-//   for (let i = nums.length; i >= 0; i--) {
-//     if (nums[i] === nums[i - 1]) {
-//       nums.splice(i, 1);
-//     }
-//   }
-//   return nums.length;
-// };
-
-console.log(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]));
-
-// Input: nums = [0,0,1,1,1,2,2,3,3,4]
-// Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
-// Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
-// It does not matter what you leave beyond the returned k (hence they are underscores).
+console.log(minSubArrayLen(7, [2, 3, 1, 2, 4, 3]));
+//                                1     3
+// Input: target = 7, nums = [2,3,1,2,4,3]
+// Output: 2
+// Explanation: The subarray [4,3] has the minimal length under the problem constraint.
